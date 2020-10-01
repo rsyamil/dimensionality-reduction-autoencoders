31e37c75d369f31debd62d55e36dac4f0e7dec5c
Syamil Mohd Razak <rsyamil@umich.edu>
2020-09-30 21:35:08 -0700

    autoencoder

---
+++ autoencoder.py
@@ -0,0 +1,196 @@
+import util
+import keras
+from keras.models import Model
+from keras.layers import Layer, Flatten, LeakyReLU
+from keras.layers import Input, Reshape, Dense, Lambda
+from keras.layers import Conv2D, MaxPooling2D, UpSampling2D
+
+from keras import backend as K
+from keras.engine.base_layer import InputSpec
+
+from keras.optimizers import Adam, SGD, RMSprop
+from keras.layers.normalization import BatchNormalization
+from keras.losses import mse, binary_crossentropy
+from keras import regularizers, activations, initializers, constraints
+from keras.constraints import Constraint
+from keras.callbacks import History 
+
+from keras.utils import plot_model
+from keras.models import load_model
+
+from keras.utils.generic_utils import get_custom_objects
+
+def sampling(args):
+    
+    epsilon_std = 1.0
+    
+    z_mean, z_log_sigma = args
+    batch = K.shape(z_mean)[0]
+    dim = K.int_shape(z_mean)[1]
+    
+    epsilon = K.random_normal(shape=(batch, dim),
+                              mean=0., stddev=epsilon_std)
+    return z_mean + K.exp(z_log_sigma) * epsilon
+
+class Autoencoder:
+
+    def __init__(self, x_sz, y_sz, z_dim= 64, variational = False, name=[]):
+        self.name = name
+        self.x_sz = x_sz
+        self.y_sz = y_sz
+        self.AE_m2m = []
+        self.AE_m2z = []
+        self.AE_z2m = []
+        self.variational = variational
+        self.z_dim = z_dim
+
+    def encoder2D(self):
+        #define the simple autoencoder
+        input_image = Input(shape=(self.x_sz, self.y_sz, 1)) 
+
+        #image encoder
+        _ = Conv2D(4, (3, 3), padding='same', name='enc')(input_image)
+        _ = LeakyReLU(alpha=0.3)(_)
+        _ = MaxPooling2D((2, 2))(_)
+
+        _ = Conv2D(8, (4, 4), padding='same')(_)
+        _ = BatchNormalization()(_)
+        _ = LeakyReLU(alpha=0.3)(_)
+        _ = MaxPooling2D((2, 2))(_)
+
+        _ = Conv2D(16, (5, 5), padding='same')(_)
+        _ = BatchNormalization()(_)
+        _ = LeakyReLU(alpha=0.3)(_)
+        _ = MaxPooling2D((2, 2))(_)
+
+        _ = Flatten()(_)
+
+        if not self.variational:
+            encoded_image = Dense(self.z_dim)(_)
+        else:
+            _ = Dense(self.z_dim)(_)
+            z_mean_m = Dense(self.z_dim)(_)
+            z_log_var_m = Dense(self.z_dim)(_)
+            encoded_image = Lambda(sampling)([z_mean_m, z_log_var_m])
+            return input_image, encoded_image, z_mean_m, z_log_var_m
+
+        return input_image, encoded_image
+
+    def decoder2D(self, encoded_image):
+        #image decoder
+        _ = Dense((256))(encoded_image)
+        _ = Reshape((4, 4, 16))(_)
+
+        _ = Conv2D(16, (5, 5), padding='same')(_)
+        _ = BatchNormalization()(_)
+        _ = LeakyReLU(alpha=0.3)(_)
+        _ = UpSampling2D((2, 2))(_)
+
+        _ = Conv2D(8, (4, 4), padding='same')(_)
+        _ = BatchNormalization()(_)
+        _ = LeakyReLU(alpha=0.3)(_)
+        _ = UpSampling2D((2, 2))(_)
+
+        _ = Conv2D(4, (3, 3))(_)
+        _ = LeakyReLU(alpha=0.3)(_)
+        _ = UpSampling2D((2, 2))(_)
+
+        decoded_image = Conv2D(1, (3, 3), padding='same')(_)
+
+        return decoded_image
+
+    def train_autoencoder2D(self, x_train, load = False):
+        #set loss function, optimizer and compile
+        input_image, encoded_image = self.encoder2D()
+        decoded_image = self.decoder2D(encoded_image)
+
+        self.AE_m2m = Model(input_image, decoded_image)
+        opt = keras.optimizers.Adam(lr=1e-3)
+        self.AE_m2m.compile(optimizer=opt, 
+                        loss="mse", 
+                        metrics=['mse'])
+
+        #get summary of architecture parameters and plot arch. diagram
+        self.AE_m2m.summary()
+        plot_model(self.AE_m2m, to_file='AE_m2m.png')
+
+        #train the neural network
+        if not load:
+            plot_losses = util.PlotLosses()
+            self.AE_m2m.fit(x_train, x_train,        
+                            epochs=100,
+                            batch_size=32,
+                            shuffle=True,
+                            validation_split=0.3,
+                            callbacks=[plot_losses])
+            #save trained model
+            self.AE_m2m.save('AE_m2m.h5')
+        else:
+            #load an already trained model
+            #some bug here, self.AE_m2z and self.AE_z2m not working
+            #when model is loaded.
+            print("Trained model loaded")
+            self.AE_m2m = load_model('AE_m2m.h5')
+
+        #set the encoder model
+        self.AE_m2z = Model(input_image, encoded_image)
+
+        #set the decoder model
+        zm_dec = Input(shape=(self.z_dim, )) 
+        _ = self.AE_m2m.layers[14](zm_dec)
+        for i in range(15, 27):
+            _ = self.AE_m2m.layers[i](_)
+        decoded_image_ = self.AE_m2m.layers[27](_)
+        self.AE_z2m = Model(zm_dec, decoded_image_)
+
+    def train_var_autoencoder2D(self, x_train, load = False):
+        #set loss function, optimizer and compile
+        input_image, encoded_image, z_mean, z_log_var = self.encoder2D()
+        decoded_image = self.decoder2D(encoded_image)
+
+        #define the variational loss and mse loss (equal weighting)
+        def vae_loss(input_image, decoded_image):
+            recons_loss = K.sum(mse(input_image, decoded_image))                
+            kl_loss = (- 0.5) * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
+            return K.mean(recons_loss + kl_loss)
+
+        #add custom loss 
+        get_custom_objects().update({"vae_loss": vae_loss})
+
+        self.AE_m2m = Model(input_image, decoded_image)
+        opt = keras.optimizers.Adam(lr=1e-3)
+        self.AE_m2m.compile(optimizer=opt, 
+                        loss=vae_loss)
+
+        #get summary of architecture parameters and plot arch. diagram
+        self.AE_m2m.summary()
+        plot_model(self.AE_m2m, to_file='AE_m2m_var.png')
+
+        #train the neural network
+        if not load:
+            plot_losses = util.PlotLosses()
+            self.AE_m2m.fit(x_train, x_train,        
+                            epochs=100,
+                            batch_size=32,
+                            shuffle=True,
+                            validation_split=0.3,
+                            callbacks=[plot_losses])
+            #save trained model
+            self.AE_m2m.save('AE_m2m_var.h5')
+        else:
+            #load an already trained model
+            print("Trained model loaded")
+            self.AE_m2m = load_model('AE_m2m_var.h5')
+
+        #set the encoder model
+        self.AE_m2z = Model(input_image, encoded_image)
+
+        #set the decoder model
+        zm_dec = Input(shape=(self.z_dim, )) 
+        _ = self.AE_m2m.layers[17](zm_dec)
+        for i in range(18, 30):
+            _ = self.AE_m2m.layers[i](_)
+        decoded_image_ = self.AE_m2m.layers[30](_)
+        self.AE_z2m = Model(zm_dec, decoded_image_)
+
+
